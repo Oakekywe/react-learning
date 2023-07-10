175d94f7d359072c4729c0953a70ef31fe38b991
oakekywe <oakekywe.zotefamily@gmail.com>
2023-07-10 15:18:37 +0630

    [new] Redux learning setup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalCSS } from "./styles/GlobalCSS";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container } from "./styles/Share";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import LoginContextProvider from "./store/LoginContentApi";
-import { RouteGuard } from "./helpers/RouteGuard";
-
-const theme = {
-  colors: {
-    primary: "aliceblue",
-  },
-};
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalCSS />
-      <Container>
-        <LoginContextProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Login />}></Route>
-              <Route path="/home" element={
-                <RouteGuard>
-                  <Home />
-                </RouteGuard>
-              }></Route>
-            </Routes>
-          </Router>
-        </LoginContextProvider>
-      </Container>
-    </ThemeProvider>
+   <div>
+   </div>
   );
 }
 
--- src/helpers/RouteGuard.js
+++
@@ -1,9 +0,0 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import { useLoginContext } from "../store/LoginContentApi";
-
-export const RouteGuard = ({ children }) => {
-  const { loggedIn } = useLoginContext();
-  if (loggedIn) return children;
-  else return <Navigate to={"/"} replace />;
-};
