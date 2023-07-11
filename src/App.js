7b2dce8c3d5aec3d1d1665a4144755c3f053396e
oakekywe <oakekywe.zotefamily@gmail.com>
2023-07-11 16:54:07 +0630

    [new] redux toolkit setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
-import {login, logout} from './stores/actions'
+import { login, logout } from "./redux/loginReducer";
 
 function App() {
-  const logged = useSelector((state) => state.loggedIn);
+  const loggedIn = useSelector((state) => state.login);
   const dispatch = useDispatch();
-
   return (
     <div>
-      <h1>{logged ? "Member" : "Guest"}</h1>
-      <button onClick={() => dispatch(login(true))}>Login</button>
-      <button onClick={() => dispatch(logout(false))}>Logout</button>
+      <h1>{loggedIn.value ? "Member" : "Guest"}</h1>
+      <button onClick={() => dispatch(login(true))}>login</button>
+      <button onClick={() => dispatch(logout(false))}>logout</button>
     </div>
   );
 }
